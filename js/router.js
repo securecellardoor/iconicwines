3406562bb0eb94dd8ab32044a7fcd4295869a77d
Benjamin Lewis <23inhouse@gmail.com>
2014-02-24 19:19:55 +1030

    add the winelist routes

---
+++ js/router.js
@@ -0,0 +1,60 @@
+App.Router.reopen({
+  location: 'hashbang',
+});
+
+App.Router.map(function() {
+  this.resource('cart', { path: '/cart' });
+
+  this.resource('merchandise', { path: '/merchandise/:anchor' });
+  this.resource('mixed-pack', { path: '/mixed-packs/:anchor' });
+  this.resource('wine', { path: '/wines/:anchor' });
+
+  this.resource('winelist', { path: '/' });
+  this.resource('winelist', { path: '/:anchor' });
+});
+
+Ember.Route.reopen({
+  activate: function() {
+    window.scrollTo(0, 0);
+  }
+});
+
+App.MerchandiseRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('merchandise', { anchor: params.anchor }).then(function(array) {
+      return array.get('firstObject');
+    });
+  },
+});
+
+App.MixedPackRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('mixedPack', { anchor: params.anchor }).then(function(array) {
+      return array.get('firstObject');
+    });
+  },
+});
+
+App.WinelistRoute = Ember.Route.extend({
+  activate: function() {
+    setTimeout(function () { $(document).scrollTop(Ember.Route.scrollPosition); }, 0);
+  },
+  deactivate: function() {
+    Ember.Route.scrollPosition = $(document).scrollTop();
+  },
+  model: function(params) {
+    var options = {}
+    if (params.anchor) { options = { anchor: params.anchor } }
+    return this.store.find('winelist', options).then(function(array) {
+      return array.get('firstObject');
+    });
+  },
+});
+
+App.WineRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('wine', { anchor: params.anchor }).then(function(array) {
+      return array.get('firstObject');
+    });
+  },
+});
